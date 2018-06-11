da4d5fa776ec74be0ba33c8ace5c0b1a63ee7374
uttekar007 <uttekar.nilesh@gmail.com>
2018-06-11 22:36:44 +0530

    Adding tableview to the view

--- TelstraExcercise/View/ViewController.swift
+++ TelstraExcercise/View/ViewController.swift
@@ -13,10 +13,23 @@ class ViewController: UIViewController {
     var fact : Facts?
     var factpresenter = FactPresenter()
     
+    let tableView : UITableView = {
+        let t = UITableView()
+        t.translatesAutoresizingMaskIntoConstraints = false
+        return t
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // set datasource
+        tableView.dataSource = self
+        
+        // register a defalut cell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        // add subviews and set it's constaints
+        self.addSubViewsAndConstraints()
         // call webservices to get facts records
         self.callFactsAPIservice()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,15 +40,55 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func addSubViewsAndConstraints() {
+        
+        self.view.addSubview(tableView)
+        
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0.0).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0.0).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0.0).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0.0).isActive = true
+    }
+    
     //Method to get data from remote server
     func callFactsAPIservice()  {
         factpresenter.getFacts(complete: { (facts) in
-            print(facts);
-            
+            self.fact = facts
+            DispatchQueue.main.async {
+                self.title = self.fact?.title
+                self.tableView.reloadData()
+            }
         }) { (error) in
             print(error.localizedDescription)
             
         }
+    }
+}
+
+
+// MARK: - Table view data source
+extension ViewController : UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        guard let fact = self.fact else { return 0 }
+        return fact.rows.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        if let ObjFact = fact?.rows[indexPath.row]
+        {
+            cell.textLabel?.text = ObjFact.title
+            cell.detailTextLabel?.text = ObjFact.description
+        }
+        return cell
     }
 }
 
